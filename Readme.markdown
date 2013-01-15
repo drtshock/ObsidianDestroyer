c74f45263555a57ddc585b1622bea0380f0bea6a
NuclearW <incongruency@gmail.com>
2013-01-15 04:25:18 -0500

    Initial commit

---
+++ Readme.markdown
@@ -0,0 +1,46 @@
+ObsidianDestroyer plugin v2.4<br>
+by Pandemoneus<br>
+https://github.com/Pandemoneus<br>
+
+Official Maintained<br>
+https://github.com/drtshock/ObsidianDestroyer
+
+How to install:
+----------------
+1. Copy 'ObsidianDestroyer.jar' into your 'plugins/' folder.
+2. Load your server
+3. Edit the newly created 'config.yml' in 'plugins/ObsidianDestroyer/' and set your preferences
+
+How to uninstall:
+-----------------
+1. Delete the folder called 'ObsidianDestroyer' in plugins/.
+2. Delete 'ObsidianDestroyer.jar'
+
+Options:
+-----------------
+Radius - determines how far around the origin of the explosion Obsidian can be destroyed (default: 3) WARNING: High values probably cause lag<br>
+FluidsProtect - set to false if you want obsidan to be able to hit by explosions from within water (default: true)<br>
+EnabledFor.TNT - set true if TNT is allowed to destroy Obsidian (default: true)<br>
+EnabledFor.Cannons - set true if you would like the cannons plugin to break obsidian (default: false) https://github.com/DerPavlov/Cannons/<br>
+EnabledFor.Creepers - set true if Creepers are allowed to destroy Obsidian (default: false)<br>
+EnabledFor.Ghasts - set true if Ghasts are allowed to destroy Obsidian (default: false)<br>
+Durability.Enabled - set true if you want to use the durability feature (default: false)<br>
+Durability.Amount - determines after how many TNT explosions in the radius the Obsidian block gets destroyed (default: 1)<br>
+Durability.ResetEnabled - set true if you want to reset the durability back to max after a certain time has passed (default: true)<br>
+Durability.ResetTime - time in milliseconds that has to pass before the durability is reset (default: '600000' (10 minutes))<br>
+Blocks.ChanceToDrop - set the chance to drop an obsidian block when it was blown up, set to 1.0 to always drop a block (default: 0.7 (70%))
+
+
+Permission nodes:
+-----------------
+obsidiandestroyer.help //makes help command available<br>
+obsidiandestroyer.config.reload //makes reload command available<br>
+obsidiandestroyer.config.info //makes info command available<br>
+obsidiandestroyer.durability.reset //makes reset command available
+
+Commands:
+-----------------
+obsidiandestroyer (alias: od) - shows the help<br>
+obsidiandestroyer reload (alias: od reload) - reloads the plugin<br>
+obsidiandestroyer info (alias: od info) - shows the currently loaded config<br>
+obsidiandestroyer reset (alias: od reset) - resets all currently saved obsidian durabilities
---
+++ src/com/pandemoneus/obsidianDestroyer/ObsidianDestroyer.java
@@ -0,0 +1,130 @@
+package com.pandemoneus.obsidianDestroyer;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import com.pandemoneus.obsidianDestroyer.Metrics;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The ObsidianDestroyer plugin.
+ * 
+ * Allows certain explosions to destroy Obsidian.
+ * 
+ * @author Pandemoneus
+ * 
+ */
+public final class ObsidianDestroyer extends JavaPlugin {
+	/**
+	 * Plugin related stuff
+	 */
+	private final ODCommands cmdExecutor = new ODCommands(this);
+	private ODConfig config = new ODConfig(this);
+	private final ODEntityListener entityListener = new ODEntityListener(this);
+	public static ObsidianDestroyer plugin;
+	public static final Logger log = Logger.getLogger("Minecraft");
+	
+
+	private static String version;
+	private static final String PLUGIN_NAME = "ObsidianDestroyer";
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onDisable() {
+		config.saveDurabilityToFile();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onEnable() {
+        PluginDescriptionFile pdfFile = getDescription();
+        version = pdfFile.getVersion();
+        
+		getCommand("obsidiandestroyer").setExecutor(cmdExecutor);
+		getCommand("od").setExecutor(cmdExecutor);
+
+		config.loadConfig();
+		entityListener.setObsidianDurability(config.loadDurabilityFromFile());
+		
+		// start Metrics
+		startMetrics();
+
+		getServer().getPluginManager().registerEvents(entityListener, this);
+		
+		// Auto updater :D
+		if(config.getAutoUpdate())
+		{
+			Updater updater = new Updater(this, "obsidiandestroyer", this.getFile(), Updater.UpdateType.DEFAULT, true);
+			updater.getLatestVersionString(); // Just so it doesn't give an error :o
+		}
+	}
+	
+	public void startMetrics() { 	
+		PluginDescriptionFile pdfFile = this.getDescription();
+		try {	
+			Metrics metrics = new Metrics(this);	
+			metrics.start();
+		} catch (IOException e) {
+			ObsidianDestroyer.log.warning("[" + pdfFile.getName() + "] Failed to submit the stats :-("); // Failed to submit the stats :-(
+		}
+	}
+
+	/**
+	 * Returns the version of the plugin.
+	 * 
+	 * @return the version of the plugin
+	 */
+	public static String getVersion() {
+		return version;
+	}
+
+	/**
+	 * Returns the name of the plugin.
+	 * 
+	 * @return the name of the plugin
+	 */
+	public static String getPluginName() {
+		return PLUGIN_NAME;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return getPluginName();
+	}
+
+	/**
+	 * Returns the config of this plugin.
+	 * 
+	 * @return the config of this plugin
+	 */
+	public ODConfig getODConfig() {
+		return config;
+	}
+
+	/**
+	 * Returns the entity listener of this plugin.
+	 * 
+	 * @return the entity listener of this plugin
+	 */
+	public ODEntityListener getListener() {
+		return entityListener;
+	}
+
+	/**
+	 * Method that handles what gets reloaded
+	 * 
+	 * @return true if everything loaded properly, otherwise false
+	 */
+	public boolean reload() {
+		return config.loadConfig();
+	}
+}
