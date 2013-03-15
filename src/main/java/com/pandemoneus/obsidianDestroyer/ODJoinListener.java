f9a05fb22425b465ddc9d3f8ecb669bccd4c71c0
Trent Hensler <thensler@wisc.edu>
2013-03-15 00:38:31 -0500

    Add spawner check for durability.
    No one likes messy code.

--- src/main/java/com/pandemoneus/obsidianDestroyer/ODJoinListener.java
+++ src/main/java/com/pandemoneus/obsidianDestroyer/ODJoinListener.java
@@ -19,8 +19,7 @@ public class ODJoinListener implements Listener
 	public ODConfig config;
 
 
-	public ODJoinListener(ObsidianDestroyer plugin)
-	{
+	public ODJoinListener(ObsidianDestroyer plugin) {
 		this.config = plugin.getODConfig();
 		this.odlistener = plugin.getListener();
 	}
@@ -32,10 +31,9 @@ public class ODJoinListener implements Listener
 	 * @param Send OP update message.
 	 */
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onJoin(PlayerJoinEvent event)
-	{
-		if(event.getPlayer().isOp() && ObsidianDestroyer.update)
-		{
+	public void onJoin(PlayerJoinEvent event) {
+		
+		if(event.getPlayer().isOp() && ObsidianDestroyer.update) {
 			event.getPlayer().sendMessage(ChatColor.DARK_PURPLE + "There is a new update for ObsidianDestroyer!");
 			event.getPlayer().sendMessage(ChatColor.DARK_PURPLE + "Version: " + ChatColor.GRAY + ObsidianDestroyer.name + 
 					ChatColor.DARK_PURPLE + " is the latest version!");
@@ -46,64 +44,57 @@ public class ODJoinListener implements Listener
 	}
 	
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onInteract(PlayerInteractEvent event)
-	{
+	public void onInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		if(player.hasPermission("obsidiandestroyer.info") 
 				&& event.getAction() == Action.LEFT_CLICK_BLOCK
-				&& config.getDurabilityEnabled())
-		{
-			if(player.getItemInHand().getAmount() > 0)
-			{
-				if(player.getItemInHand().getTypeId() == config.getCheckItemId())
-				{
+				&& config.getDurabilityEnabled()) {
+			if(player.getItemInHand().getAmount() > 0) {
+				if(player.getItemInHand().getTypeId() == config.getCheckItemId()) {
+					
 					Block block = event.getClickedBlock();
 					Location loc = block.getLocation();
 					if(block.getType() == Material.OBSIDIAN 
 							|| block.getType() == Material.ENDER_CHEST 
 							|| block.getType() == Material.ANVIL
-							|| block.getType() == Material.ENCHANTMENT_TABLE)
-					{
+							|| block.getType() == Material.ENCHANTMENT_TABLE) {
+						
 						Integer representation = Integer.valueOf(loc.getWorld().hashCode() + loc.getBlockX() * 2389 + loc.getBlockY() * 4027 + loc.getBlockZ() * 2053);
-						if(odlistener.obsidianDurability.containsKey(representation))
-						{
+						
+						if(odlistener.obsidianDurability.containsKey(representation)) {
+							
 							int currentDurability = ((Integer)odlistener.obsidianDurability.get(representation)).intValue();
-							if (block.getType() == Material.OBSIDIAN) 
-							{
+							
+							if (block.getType() == Material.OBSIDIAN)
 							player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this obsidian block is: " + ChatColor.WHITE + (config.getoDurability() - currentDurability) + "/" + config.getoDurability());
-							}
-							if (block.getType() == Material.ENDER_CHEST) 
-							{
+							
+							if (block.getType() == Material.ENDER_CHEST)
 								player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this ender chest is: " + ChatColor.WHITE + (config.getecDurability() - currentDurability) + "/" + config.getecDurability());
-							}
-							if (block.getType() == Material.ANVIL) 
-							{
+							
+							if (block.getType() == Material.ANVIL)
 								player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this anvil is: " + ChatColor.WHITE + (config.getaDurability() - currentDurability) + "/" + config.getaDurability());
-							}
-							if (block.getType() == Material.ENCHANTMENT_TABLE) 
-							{
+							
+							if (block.getType() == Material.ENCHANTMENT_TABLE)
 								player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this enchantment table is: " + ChatColor.WHITE + (config.geteDurability() - currentDurability) + "/" + config.geteDurability());
-							}
+							
+							if (block.getType() == Material.MOB_SPAWNER)
+								player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this spawner is: " + ChatColor.WHITE + (config.getmDurability() - currentDurability) + "/" + config.getmDurability());
+							
 							return;
-						}
-						else
-						{
+							
+						} else {
 							if (block.getType() == Material.OBSIDIAN) 
-							{
 							player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this obsidian block is: " + ChatColor.WHITE + config.getoDurability() + "/" + config.getoDurability());
-							}
+							
 							if (block.getType() == Material.ENDER_CHEST) 
-							{
 								player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this ender chest is: " + ChatColor.WHITE + config.getecDurability() + "/" + config.getecDurability());
-							}
+							
 							if (block.getType() == Material.ANVIL) 
-							{
 								player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this anvil is: " + ChatColor.WHITE + config.getaDurability() + "/" + config.getaDurability());
-							}
+							
 							if (block.getType() == Material.ENCHANTMENT_TABLE) 
-							{
 								player.sendMessage(ChatColor.DARK_PURPLE + "Durability of this enchantment table is: " + ChatColor.WHITE + config.geteDurability() + "/" + config.geteDurability());
-							}
+							
 							return;
 						}
 					}
