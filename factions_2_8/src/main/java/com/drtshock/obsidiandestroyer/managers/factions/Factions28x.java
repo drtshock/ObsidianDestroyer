fa3ed4a93f210f99de40ad2a4957fdddf4c5f92e
hexosse <hexosse@gmail.com>
2015-07-26 13:33:55 +0200

    Update to Factions 2.8

---
+++ factions_2_8/src/main/java/com/drtshock/obsidiandestroyer/managers/factions/Factions28x.java
@@ -0,0 +1,37 @@
+package com.drtshock.obsidiandestroyer.managers.factions;
+
+import com.massivecraft.factions.entity.BoardColl;
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.MFlag;
+import com.massivecraft.massivecore.ps.PS;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+
+public class Factions28x implements FactionsHook {
+
+    @Override
+    public boolean isFactionOffline(Location loc) {
+        Faction faction = BoardColl.get().getFactionAt(PS.valueOf(loc));
+        if ((faction.isNone()) ||
+                ChatColor.stripColor(faction.getName()).equalsIgnoreCase("safezone") ||
+                ChatColor.stripColor(faction.getName()).equalsIgnoreCase("warzone")) {
+            //ObsidianDestroyer.debug("Factions25x.isFactionOffline: false");
+            return false;
+        }
+        //ObsidianDestroyer.debug("Factions25x.isFactionOffline: " + faction.isFactionConsideredOffline());
+        return faction.isFactionConsideredOffline() && faction.getFlag(MFlag.ID_OFFLINEEXPLOSIONS);
+    }
+
+    @Override
+    public boolean isExplosionsEnabled(Location loc) {
+        Faction faction = BoardColl.get().getFactionAt(PS.valueOf(loc));
+        //ObsidianDestroyer.debug("Factions25x.isExplosionsEnabled: " + faction.getFlag(FFlag.EXPLOSIONS));
+        return faction == null || faction.getFlag(MFlag.ID_EXPLOSIONS);
+    }
+
+    @Override
+    public String getVersion() {
+        return "2.8.X";
+    }
+
+}
