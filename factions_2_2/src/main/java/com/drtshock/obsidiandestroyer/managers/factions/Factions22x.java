c09ae34072db34ff8fbfc43e8ead55551b691b79
squidicuz <jono324@gmail.com>
2015-12-31 16:20:22 -0500

    Fix version issues with factions.  Support for 2.2.x and 2.5.x fixed.

--- factions_2_2/src/main/java/com/drtshock/obsidiandestroyer/managers/factions/Factions22x.java
+++ factions_2_2/src/main/java/com/drtshock/obsidiandestroyer/managers/factions/Factions22x.java
@@ -3,11 +3,11 @@ package com.drtshock.obsidiandestroyer.managers.factions;
 import com.massivecraft.factions.FFlag;
 import com.massivecraft.factions.entity.BoardColls;
 import com.massivecraft.factions.entity.Faction;
-import com.massivecraft.massivecore.ps.PS;
+import com.massivecraft.mcore.ps.PS;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 
-public class Factions25x implements FactionsHook {
+public class Factions22x implements FactionsHook {
 
     @Override
     public boolean isFactionOffline(Location loc) {
@@ -15,23 +15,23 @@ public class Factions25x implements FactionsHook {
         if (faction.isNone() ||
                 ChatColor.stripColor(faction.getName()).equalsIgnoreCase("safezone") ||
                 ChatColor.stripColor(faction.getName()).equalsIgnoreCase("warzone")) {
-            //ObsidianDestroyer.debug("Factions25x.isFactionOffline: false");
+            //ObsidianDestroyer.debug("Factions22x.isFactionOffline: false");
             return false;
         }
-        //ObsidianDestroyer.debug("Factions25x.isFactionOffline: " + faction.isFactionConsideredOffline());
+        //ObsidianDestroyer.debug("Factions22x.isFactionOffline: " + faction.isFactionConsideredOffline());
         return faction.isFactionConsideredOffline() && faction.getFlag(FFlag.OFFLINE_EXPLOSIONS);
     }
 
     @Override
     public boolean isExplosionsEnabled(Location loc) {
         Faction faction = BoardColls.get().getFactionAt(PS.valueOf(loc));
-        //ObsidianDestroyer.debug("Factions25x.isExplosionsEnabled: " + faction.getFlag(FFlag.EXPLOSIONS));
+        //ObsidianDestroyer.debug("Factions22x.isExplosionsEnabled: " + faction.getFlag(FFlag.EXPLOSIONS));
         return faction.getFlag(FFlag.EXPLOSIONS);
     }
 
     @Override
     public String getVersion() {
-        return "2.5.X";
+        return "2.2.X";
     }
 
 }
